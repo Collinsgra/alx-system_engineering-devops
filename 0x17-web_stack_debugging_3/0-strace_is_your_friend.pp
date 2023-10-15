16b8e3a35263d61d0f4d573e6d7017e1c919f99c
Collinsgra <kipkemoicollins941@gmail.com>
2023-10-15 14:58:24 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,10 @@
-# Fixed500 error.
-exec{'fix-php':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => '/usr/local/bin/:/bin/'
+#puppet lines in a line
+
+$phpp_to_php = '/var/www/html/wp-settings.php'
+
+#replace "phpp" with "php"
+
+exec { 'replace_phpp':
+  command => "sed -i 's/phpp/php/g' ${phpp_to_php}",
+  path    => ['/bin','/usr/bin']
 }
